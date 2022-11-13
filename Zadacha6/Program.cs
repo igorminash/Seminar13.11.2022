a317291c4fc05ef03563c0a1f16e92cfc64d0d3b
igorminash <iminash@inbox.ru>
2022-11-13 17:19:40 +0300

    Добавлено решение задачи 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+
+if (A % 2 == 0)
+{
+    Console.WriteLine($"Число {A} - чётное");
+
+}
+else
+{
+    Console.WriteLine($"Число {A} - нечётное");
+}
+
+
