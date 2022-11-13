f33574487274711a99d9f2410543b4475e39effb
igorminash <iminash@inbox.ru>
2022-11-13 16:54:56 +0300

    Добавлено решение задачи 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B)
+{
+    Console.WriteLine($"Большее число: {A}");
+}
+else
+{
+    Console.WriteLine($"Большее число: {B}");
+}
+
