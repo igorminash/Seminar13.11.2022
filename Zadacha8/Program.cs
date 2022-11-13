98cdad5a32728fc201b9d8e12dd9889ddb54fed0
igorminash <iminash@inbox.ru>
2022-11-13 17:37:06 +0300

    Добавлено решение задачи 8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int number = 1;
+
+while (number <= N)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);  
+    }
+    number ++;
+}
